07501b2424d083771f805b1c6d27df72738215cf
claudsaints <claudiodsantoss7@gmail.com>
2024-06-24 14:09:22 -0300

    functions for mobile

--- js/script.js
+++ js/script.js
@@ -119,6 +119,27 @@ const nDraw = (x,y) => {
     contexto.stroke();
 }
 
-//configuração android
+//configuração mobile
 
 
+canvas.addEventListener("touchstart", function(e){
+    var touch = e.touches[0]
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX:touch.clientX,
+        clientY:touch.clientY
+    })
+    canvas.dispatchEvent(mouseEvent)
+},false);
+
+canvas.addEventListener("touchend", function(e){
+    var mouseEvent = new MouseEvent("mouseup",{});
+    canvas.dispatchEvent(mouseEvent)
+},false)
+canvas.addEventListener("touchmove", function(e){
+    var touch = e.touches[0]
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX:touch.clientX,
+        clientY:touch.clientY
+    })
+    canvas.dispatchEvent(mouseEvent)
+},false)
